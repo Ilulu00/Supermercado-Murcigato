df636b2a33c1d3fdbd0517732f9219a557916afc
Ilulu00 <florezsantiago773@gmail.com>
2025-11-01 15:38:33 -0500

    Feat: adicion del campo "activo" en la tabla factura

--- Entidades/Factura.py
+++ Entidades/Factura.py
@@ -20,9 +20,10 @@ class Factura(Base):
     Modelo de las columnas que haran parte de la clase factura, la cual servira como base para crear, y posterior mostrar esta factura.
 
         id_factura: El identificador único de cada factura
-        metodo_pago:
-        subtotal:
-        total:
+        metodo_pago: con que metodo pago el usuario
+        subtotal: el total de los productos sin descuento
+        total: el total de los productos con descuento
+        activo: muestra si la factura ya se cancelo o sigue vigente de pago
         descuento: Si al final se aplico un descuento o no, siguiendo ciertas reglas.
         id_carrito: Para mostrar los productos que se compro.
     """
@@ -35,6 +36,7 @@ class Factura(Base):
     metodo_pago = Column(String, nullable=False)
     subtotal = Column(Float, nullable=True)
     total = Column(Float, nullable=True)
+    activo = Column(bool, nullable=False)
     descuento = Column(Float, nullable=True)
     id_carrito = Column(
         UUID(as_uuid=True), ForeignKey("Carrito.id_carrito"), nullable=False
@@ -58,6 +60,7 @@ class Factura(Base):
             f"Subtotal: {self.subtotal}\n"
             f"Total: {self.total}\n"
             f"Descuento aplicado: {self.descuento}"
+            f"Estado: {self.activo}\n"
         )
 
     def to_dict(self):
